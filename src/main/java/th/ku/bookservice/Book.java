27338b8c6fb41ac38d1a38eedafa158cd2d895d7
folk <talesrunnerfolk@gmail.com>
2019-09-23 12:41:52 +0700

    add

---
+++ src/main/java/th/ku/bookservice/Book.java
@@ -0,0 +1,36 @@
+package th.ku.bookservice;
+
+public class Book {
+    private int id;
+    private String name;
+    private double price;
+
+    public Book(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // …. getters and setters …..
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price + '}';
+    }
+}
+
---
+++ src/main/java/th/ku/bookservice/BookService.java
@@ -0,0 +1,34 @@
+package th.ku.bookservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/book"})
+public class BookService {
+
+    @Autowired
+    private BookDaoImp repository;
+
+    @GetMapping
+    public List<Book> findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable int id) {
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    public Book create(@RequestBody Book book){
+        repository.save(book);
+        return repository.findById(book.getId());
+    }
+
+
+
+}
+
